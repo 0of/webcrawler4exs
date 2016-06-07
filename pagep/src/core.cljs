ea40cc33b100b37159db87623888560abdba7da8
magnus <Magnusbackyard@live.com>
2016-06-07 16:52:49 +0800

    apply electron instead of nwjs

--- pagep/src/core.cljs
+++ pagep/src/core.cljs
@@ -2,39 +2,29 @@
 
 (enable-console-print!)
 
+(defn new-window
+  []
+  (let [BrowserWindow (-> "electron"
+                          js/require
+                          .-BrowserWindow)]
+    (BrowserWindow. (clj->js {:webPreferences {:nodeIntegration true
+                                               :webgl false}}))))
 (defn load
   [url on-load]
   "load from url and return a `Window` object"
-  (let [window (-> "nw.gui"
-                   js/require
-                   .-Window)
-        on-opened (fn [opened] (.on opened "loaded" #(on-load opened)))]
-    (.open window url #js {:new_instance false} on-opened)))
+  (let [window (new-window)]
+    (.loadURL window url)
+    (.on (.-webContents window) "did-finish-load" #(.executeJavaScript (.-webContents window) "require('electron').ipcRenderer.send('chan0', document.body.innerHTML);"))))
 
-(defn snapshot
-  [window on-finished]
-  (.capturePage window on-finished #js {:format "png"
-                                        :datatype "buffer"}))
-
-(defn- filter-links
-  [el]
-  (let [href (.getAttribute el "href")]
-    (and href
-         (not= 0 (.-length href))
-         (not= 0 (.indexOf href "javascript:"))
-         (not= 0 (.indexOf href "#")))))
-
-(defn crawl-links
-  [window]
-  "access doc and search <a href>"
-  (let [doc (aget window "window" "document")]
-    (->> (.getElementsByTagName doc "a")
-         array-seq
-         (filter filter-links)
-         (map #(.-href %)))))
+(defn- on-app-ready4
+  []
+  (let [Ipc (.-ipcMain (js/require "electron"))]
+    (.on Ipc "chan0" #(prn %))
+    (load "" nil)))
 
 (defn -main
   [& args]
-  (load "" (fn [window] (.showDevTools window) (prn (crawl-links window)))))
+  (let [App (.-app (js/require "electron"))]
+    (.on App "ready" on-app-ready)))
 
-(-main)
+(set! *main-cli-fn* -main)
