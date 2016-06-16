c1569eb210f80ae1900b8e8c89efb5bccea85e57
Magnus <Magnusbackyard@live.com>
2016-06-16 08:03:34 +0800

    crawling task

--- lib/crawling-task.ex
+++ lib/crawling-task.ex
@@ -3,11 +3,10 @@ defmodule CrawlingTask do
   # start the web page processing container
   #
   def run(url) do
-    load_url_env = String.join(["LOAD_URL", url], "=")
+    load_url_env = Enum.join(["LOAD_URL", url], "=")
 
-    command = String.join(["docker", "run",
-                           "--read-only", "-v", "/opt/electron/electron:/", "/opt/pagep/:/",
-                           "--env", load_url_env], " ")
-    System.cmd(command)
+    System.cmd("docker", ["run",
+                          "--read-only", "-v", "/opt/electron/electron:/", "/opt/pagep/:/",
+                          "--env", load_url_env])
   end
 end
